8f2c96e2286ba415bec44de3b16134aa0d9dd5a1
Tudor Enache <tudor.enache7@gmail.com>
2025-04-02 12:02:41 +0200

    Tested linear module further. Found some problems (but they probably come from the optimizer).

--- linear.py
+++ linear.py
@@ -10,7 +10,7 @@ class MSELoss(Loss):
         # y: batch x 1
         # yhat: batch x 1
         # output: 1
-        return ((y - yhat) ** 2).sum(axis=1).mean()
+        return np.mean((y - yhat) ** 2)
     
     def backward(self, y, yhat):
         # output: batch x 1
@@ -18,20 +18,21 @@ class MSELoss(Loss):
 
 class Linear(Module):
     def __init__(self, input_dim, output_dim):
-        self._parameters = {"w": np.random.randn(input_dim, output_dim),
-                            "b": np.random.randn(1, output_dim)}
+        self._parameters = {"w": np.random.randn(input_dim, output_dim) * 0.01,
+                            "b": np.zeros((1, output_dim))}
         self._gradient = {"w": None, "b": None}
         self._input = None
 
     def zero_grad(self):
-        self._gradient["w"] = 0
-        self._gradient["b"] = 0
+        self._gradient["w"] = np.zeros_like(self._parameters["w"])
+        self._gradient["b"] = np.zeros_like(self._parameters["b"])
 
     def forward(self, x):
         self._input = x
         return x @ self._parameters["w"] + self._parameters["b"]
     
     def backward_update_gradient(self, delta):
+        # ADD SOME NORMALIZATION
         x = self._input
         self._gradient["w"] += x.T @ delta
         self._gradient["b"] += np.sum(delta, axis=0, keepdims=True)
--- test_linear.py
+++ test_linear.py
@@ -5,13 +5,15 @@ from linear import *
 # add some noise to the output
 # try to learn it with batch-GD
 
-a = 2
-x = np.arange(1, 101)
-y = a * x + np.random.normal(size=x.shape)
-x = x.reshape(x.shape[0], 1)
-y = y.reshape(y.shape[0], 1)
+# choose a number of points uniformly in the range [0, 50]
+a = 3
+b = 10
+x = np.random.uniform(0, 20, (1000000, 1))
+y = a * x + b + np.random.normal(size=x.shape)
+print(x.shape)
+print(y.shape)
 
-batch = 5
+batch = 10
 x_batches = x.reshape(x.shape[0] // batch, batch, 1)
 y_batches = y.reshape(y.shape[0] // batch, batch, 1)
 
@@ -19,6 +21,8 @@ M = Linear(1, 1)
 L = MSELoss()
 
 losses = []
+weights = []
+biases = []
 for i in range(len(x_batches)):
     x_batch = x_batches[i]
     y_batch = y_batches[i]
@@ -31,12 +35,32 @@ for i in range(len(x_batches)):
     delta = L.backward(y_batch, y_hat_batch)
     M.backward_update_gradient(delta)
     M.update_parameters()
+    weights.append(M._parameters["w"][0][0])
+    biases.append(M._parameters["b"][0][0])
 
 y_hat = M.forward(x)
 print(M)
 
 import matplotlib.pyplot as plt
-plt.plot(x, y, color="r", label="True data")
+plt.scatter(x, y, color="r", label="True data")
 plt.plot(x, y_hat, color="b", label="Predicted data")
 plt.legend()
+plt.title("Regression")
 plt.show()
+
+plt.scatter(np.arange(len(x_batches)), weights)
+plt.title("Weights")
+plt.show()
+
+plt.scatter(np.arange(len(x_batches)), biases)
+plt.title("Biases")
+plt.show()
+
+plt.scatter(np.arange(len(x_batches)), losses)
+plt.title("Loss")
+plt.show()
+
+# Problems noticed:
+# 1. If we keep increasing the range of the training data the weights don't adapt fast enough
+# 2. Bias is not learned at the same rate as the weights
+# => this is probably a problem with our optimizer
