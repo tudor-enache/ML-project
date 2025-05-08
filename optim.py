94100c17757b16fa2b19756b5a169d82d298e26a
athelc <chry.elise@gmail.com>
2025-05-08 15:33:22 +0200

    modif in opt shuffle

--- optim.py
+++ optim.py
@@ -1,6 +1,21 @@
 import numpy as np
 from projet_etu import Loss, Module
 from sequential import *
+import random
+
+def shuffle(X,Y):
+    N = X.shape[0]
+    X_sh = []
+    Y_sh = []
+
+    indeces = random.sample(range(N),N)
+
+    for ind in indeces :
+        X_sh.append(X[ind])
+        Y_sh.append(Y[ind])
+    
+    return np.array(X_sh),np.array(Y_sh)
+
 
 class Optim(object):
     def __init__(self,net,loss_fn,eps):
@@ -19,7 +34,7 @@ class Optim(object):
         loss = self.loss_fn.forward(batch_y, y_hat)
         
         #if len(loss)> 1 : loss = sum(loss)
-        print("The loss is",loss)
+        #print("The loss is",loss)
         self.losses.append(loss)
         
         self.net.zero_grad()
@@ -36,6 +51,7 @@ class Optim(object):
     def SGD(self,net2,x,y,batch_nb,num_epochs):
 
         #TODO: chnage it so the batches are selected randomly
+        x,y = shuffle(x,y)
         x_batches = x.reshape(x.shape[0] // batch_nb, batch_nb, x.shape[1])
         
         n = 1 if len(y.shape) < 2 else y.shape[1]
